1b23a0df8c15b43922ad157eacc8876c0dcbaf44
simongalaxy <simongalaxy2011@gmail.com>
2025-11-12 20:28:54 +0800

    updated llm_scraper.py

--- tools/llm_scraper.py
+++ tools/llm_scraper.py
@@ -1,5 +1,33 @@
-from langchain_ollama import OllamaLLM
+from langchain_classic.schema import Document
 from langchain_community.document_loaders import PlaywrightURLLoader
+
+from bs4 import BeautifulSoup
+
+# Render Page.
+def render_job_page(urls: list[str]) -> list[Document]:
+    
+    loader = PlaywrightURLLoader(urls=urls)
+    docs = loader.load()
+    
+    return docs
+
+
+# post-process with beautifulsoup.
+def post_process_rendered_page(docs: list[Document]) -> list[str]:
+    
+    clean_texts = []
+    
+    for doc in docs:
+        soup = BeautifulSoup(doc.page_content, "html.parser")
+        job_desc = soup.find("div", class_="jd__content")
+        
+        print(job_desc)
+        # clean_text = job_desc.get_text(strip=True)
+        # print(clean_text)
+        # clean_texts.append(clean_text)
+    
+    return clean_texts
+
 
 
 # scrape job responsibilities, job requirements from job advertisement link. 
@@ -8,19 +36,13 @@ def get_job_ad_data(jobs: list[dict]) -> None:
     # copy jobs.
     copy_jobs = jobs
     
-    # initiate llm using ollama.
-    # llm = OllamaLLM(model="llama3")
-    
+    # get the job ad urls.
     job_urls = [job["jobUrl"] for job in copy_jobs]
-    print(job_urls)
     
-    loader = PlaywrightURLLoader(
-        urls=job_urls,
-        headless=True
-        )
-    docs = loader.load()
-    
-    print(docs[0].page_content)
+    # render job ad pages to html and then generate clean text.
+    docs = render_job_page(urls=job_urls)
+   
+    clean_texts = post_process_rendered_page(docs=docs)
     
     return
 
